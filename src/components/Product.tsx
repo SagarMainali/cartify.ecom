4417a7694ec429be44eac261d9788dd9f9361673
Sagar Mainali <sagarmainali78@gmail.com>
2023-05-05 19:58:40 +0545

    final fix of products button alignment

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,27 +1,23 @@
 
 export default function Product({ id, title, image }: { id: number, title: string, image: string }) {
 
-     const counter = 1
+     const counter = 0
 
      return (
-          <div className="product p-4 bg-white border-2 hover:bg-slate-50 text-sm flex flex-col gap-3">
-               <div className="details mb-auto flex flex-col items-center gap-3">
-                    <div>
-                         <img src={image} alt={`product${id}`} className="object-cover h-[9rem]" />
+          <div className="product p-4 bg-white border-2 hover:bg-slate-50 text-sm flex flex-col gap-4 items-center">
+               <div>
+                    <img src={image} alt={`product${id}`} className="object-cover h-[9rem]" />
+               </div>
+               <hr className="w-full" />
+               <h2>{title}</h2>
+               {counter < 1
+                    ? <button className="bg-blue-500 py-2 text-white rounded-md mt-auto w-full">Add to cart</button>
+                    : <div className="bottom-3 flex items-center mt-auto">
+                         <button className="flex justify-center items-center bg-gray-300 rounded-full h-10 w-10">-</button>
+                         <span className="px-3">3 in cart</span>
+                         <button className="flex justify-center items-center bg-gray-300 rounded-full h-10 w-10">+</button>
                     </div>
-                    <hr className="w-full" />
-                    <h2 className="">{title}</h2>
-               </div>
-               <div className="btn-container flex justify-center">
-                    {counter < 1
-                         ? <button className="bg-blue-500 py-2 px-4 text-white rounded-md">Add to cart</button>
-                         : <div className="bottom-3 flex items-center">
-                              <button className="flex justify-center items-center bg-gray-300 rounded-full h-10 w-10">-</button>
-                              <span className="px-3">3 in cart</span>
-                              <button className="flex justify-center items-center bg-gray-300 rounded-full h-10 w-10">+</button>
-                         </div>
-                    }
-               </div>
+               }
           </div>
      )
 }
