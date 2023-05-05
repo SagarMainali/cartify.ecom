a40e608f4fc4f5aa331c130c235745569b0f48f8
Sagar Mainali <sagarmainali78@gmail.com>
2023-05-05 19:38:45 +0545

    added buttons on product

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,11 +1,23 @@
 
 export default function Product({ id, title, image }: { id: number, title: string, image: string }) {
+
+     const counter = 1
+
      return (
-          <div className="product p-2 bg-white border-2">
-               <div className="flex flex-col items-center mb-3">
+          <div className="product p-4 bg-white border-2 hover:bg-slate-50 text-sm flex flex-col gap-3 items-center relative">
+               <div>
                     <img src={image} alt={`product${id}`} className="object-cover h-[9rem]" />
                </div>
-               <h2>{title}</h2>
+               <hr className="w-full" />
+               <h2 className="mb-12">{title}</h2>
+               {counter < 1
+                    ? <button className="bg-blue-500 py-2 px-4 text-white rounded-md absolute bottom-3">Add to cart</button>
+                    : <div className="btn-container absolute bottom-3 flex items-center">
+                         <button className="flex justify-center items-center bg-gray-300 rounded-full h-10 w-10">-</button>
+                         <span className="px-3">3 in cart</span>
+                         <button className="flex justify-center items-center bg-gray-300 rounded-full h-10 w-10">+</button>
+                    </div>
+               }
           </div>
      )
 }
