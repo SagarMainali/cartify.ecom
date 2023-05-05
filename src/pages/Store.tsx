2b3ff3703b240d974ad656bc77a3d45b4a6722be
Sagar Mainali <sagarmainali78@gmail.com>
2023-05-05 17:25:15 +0545

    updated svg icon

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,8 +1,17 @@
+import { useEffect, useState } from 'react'
 
 export default function Store() {
+
+     const [data, setData] = useState()
+
+     useEffect(() => {
+          fetch('https://fakestoreapi.com/products/category/jewelery/?limit=2')
+               .then(res => res.json())
+               .then(json => setData(json))
+     }, [])
      return (
           <div className="store">
-
+               {data ? <h2>{JSON.stringify(data)}</h2> : <h2>getting products...</h2>}
           </div>
      )
 }
