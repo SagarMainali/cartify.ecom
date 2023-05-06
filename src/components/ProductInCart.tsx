22bfa55ee401ee32fa06289fcdc95bba41c62876
Sagar Mainali <sagarmainali78@gmail.com>
2023-05-06 14:18:23 +0545

    font size adjustment

--- src/components/ProductInCart.tsx
+++ src/components/ProductInCart.tsx
@@ -7,9 +7,9 @@ export default function ProductInCart({ image, id, title, price, cartQuantity }:
                     <img src={image} alt={`product${id}`} className="object-cover h-[6.5rem]" />
                </div>
                <div className="details">
-                    <h2 className="leading-5 mb-2">{title}</h2>
-                    <h2>Quantity <span className="font-semibold text-sm">({cartQuantity})</span></h2>
-                    <h2>Price <span className="font-semibold text-sm">${(price * cartQuantity).toFixed(2)}</span></h2>
+                    <h2 className="leading-5 mb-2 text-sm sm:text-base">{title}</h2>
+                    <h2 className="text-sm">Quantity <span className="font-semibold">({cartQuantity})</span></h2>
+                    <h2 className="text-sm">Price <span className="font-semibold">${(price * cartQuantity).toFixed(2)}</span></h2>
                </div>
           </div>
      )
