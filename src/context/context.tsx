5c0a448c5fb3c5f1a1bca1b8c5155dd05a80c1f3
Sagar Mainali <sagarmainali78@gmail.com>
2023-05-06 16:46:17 +0545

    added localstorage functionality

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -20,51 +20,59 @@ export function ShoppingCartContextProvider({ children }: { children: ReactNode 
      const [data, setData] = useState<ProductType[]>([])
 
      useEffect(() => {
-          fetch(`https://fakestoreapi.com/products/category/electronics?limit=6`)
-               .then(response => response.json())
-               .then(parsedData => {
-                    return parsedData.map((item: ProductType) => ({
-                         ...item,
-                         cartQuantity: 0
+          const fetchProducts = async () => {
+               try {
+                    const response = await fetch(`https://fakestoreapi.com/products/category/electronics?limit=6`)
+                    const parsedData = await response.json()
+                    const updatedData = parsedData.map((item: ProductType) => (
+                         {
+                              ...item,
+                              cartQuantity: 0
+                         }
+                    ));
+                    localStorage.setItem('shopping_cart_data', JSON.stringify(updatedData))
+                    setData(updatedData)
+               } catch (error) {
+                    console.log(error)
+               }
+          }
 
-                    }))
-               })
-               .then((updatedData: ProductType[]) => setData(updatedData))
-               .catch(error => console.log(error))
+          const localData = localStorage.getItem('shopping_cart_data')
+          if (localData) {
+               setData(JSON.parse(localData))
+          } else {
+               fetchProducts()
+          }
      }, [])
 
      function addToCart(id: number): void {
-          setData(
-               (prevState: ProductType[]) => (
-                    prevState.map(
-                         (item: ProductType) => (
-                              item.id === id
-                                   ? {
-                                        ...item,
-                                        cartQuantity: item.cartQuantity + 1
-                                   }
-                                   : item
-                         )
-                    )
+          const updatedData = data.map(
+               (item: ProductType) => (
+                    item.id === id
+                         ? {
+                              ...item,
+                              cartQuantity: item.cartQuantity + 1
+                         }
+                         : item
                )
           )
+          localStorage.setItem('shopping_cart_data', JSON.stringify(updatedData))
+          setData(updatedData)
      }
 
      function removeFromCart(id: number): void {
-          setData(
-               (prevState: ProductType[]) => (
-                    prevState.map(
-                         (item: ProductType) => (
-                              item.id === id
-                                   ? {
-                                        ...item,
-                                        cartQuantity: item.cartQuantity - 1
-                                   }
-                                   : item
-                         )
-                    )
+          const updatedData = data.map(
+               (item: ProductType) => (
+                    item.id === id
+                         ? {
+                              ...item,
+                              cartQuantity: item.cartQuantity - 1
+                         }
+                         : item
                )
           )
+          localStorage.setItem('shopping_cart_data', JSON.stringify(updatedData))
+          setData(updatedData)
      }
 
      return (
