401c106ef835c4b16533ef95b93b6e5e4721c2c9
Sagar Mainali <sagarmainali78@gmail.com>
2023-05-06 15:04:50 +0545

    fixed decimal value in price

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -20,7 +20,7 @@ export default function Cart() {
                <hr className="border-2 border-gray-300" />
                {itemsInCart.length > 0 &&
                     <div className="checkout text-end flex gap-3 items-center justify-end bg-slate-200 rounded-md px-4 py-2">
-                         <h2>Your total: <span className="font-bold">${totalPrice.reduce((result, number) => result + number)}</span></h2>
+                         <h2>Your total: <span className="font-bold">${(totalPrice.reduce((result, number) => result + number)).toFixed(2)}</span></h2>
                          <button className="px-4 py-2 bg-blue-600 text-white rounded-lg">Checkout</button>
                     </div>
                }
