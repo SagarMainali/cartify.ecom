32ad5645909909514a8b81d0da8792da7a0cdbd6
Sagar Mainali <sagarmainali78@gmail.com>
2023-05-08 17:40:57 +0545

    added firebase and its config file

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+     apiKey: "AIzaSyAtTXkmseLsLXpXEijQcaOoizs_IhcK9Sw",
+     authDomain: "cartify-10415.firebaseapp.com",
+     projectId: "cartify-10415",
+     storageBucket: "cartify-10415.appspot.com",
+     messagingSenderId: "499966273448",
+     appId: "1:499966273448:web:ec76e99a532be5a38f5f9d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
