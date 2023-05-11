8faa6e6490065f1cc44713e60c699c27b7b71c5a
Sagar Mainali <sagarmainali78@gmail.com>
2023-05-11 15:58:56 +0545

    added firestore sdk

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
      apiKey: "AIzaSyAtTXkmseLsLXpXEijQcaOoizs_IhcK9Sw",
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 const firebase_Project = initializeApp(firebaseConfig)
 export const auth = getAuth(firebase_Project)
+export const firestore = getFirestore(firebase_Project)
